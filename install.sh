aeafee10898b2dc80050ffad83b0aa3a25b2362b
aun <aun.sira@gmail.com>
2016-09-21 00:18:20 +0700

    Update install script to symlink vim files

--- install.sh
+++ install.sh
@@ -14,3 +14,15 @@ if [[ "$symlink_answer" == "y" ]]; then
     echo -e "${HOME}/.$fl \t→\t $basedir/$fl"
   done
 fi
+
+_vimfiles_path=$basedir/vim;
+read -n1 -p "Symlink all vim custom files to ~/.vim/custom/ (overwriting)? (y/n)" symlink_vim_answer
+echo ""
+if [[ "$symlink_vim_answer" == "y" ]]; then
+  for vfl in "$_vimfiles_path"/*; do
+    vim_file=`basename "$vfl"`
+    echo $vim_file
+    ln -sfn $basedir/vim/$vim_file ${HOME}/.vim/custom/$vim_file
+    echo -e ${HOME}/.vim/custom/$vim_file \t→\t $basedir/vim/$vim_file
+  done
+fi
