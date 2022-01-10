2b2d3d41828525555c45605ca2b9f5f46b1005d5
aun <aun.sira@gmail.com>
2022-01-10 18:23:43 +0700

    lsp: C-space to cmp.mapping.close()

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -71,7 +71,7 @@ cmp.setup {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    -- ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.mapping.close(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
