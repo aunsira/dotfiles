bb16b1a9ce3031a70864515d360def960194021d
aun <aun.sira@gmail.com>
2022-12-26 13:27:51 +0700

    vim: Enable completion on tab

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -60,7 +60,17 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup {
+  completion = {
+    autocomplete = false,
+  },
   snippet = {
     expand = function(args)
       require('snippy').expand_snippet(args.body)
@@ -79,6 +89,8 @@ cmp.setup {
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
